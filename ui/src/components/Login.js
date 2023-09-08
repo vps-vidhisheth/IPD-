cb3df95f57345ef27bb297e6adb2000aca3d4440
jatin-pandey01 <=>
2023-09-08 21:41:48 +0530

    Dark mode done

--- ui/src/components/Login.js
+++ ui/src/components/Login.js
@@ -1,14 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Footer from "./Footer";
-import Navbar from "./Navbar";
+import loginIcon from './../assets/login_icon_prev_ui.png';
+import loginWoman from './../assets/login_women_prev_ui.png';
+import {AiFillEyeInvisible,AiFillEye} from "react-icons/ai";
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [eyeChecker,setEyeChecker] = useState(false);
+  const [password,setPassword] = useState('');
+  const inputType = 'password';
+  const changeEye = ()=>{
+    console.log(eyeChecker);
+    setEyeChecker(!eyeChecker);
+  }
+
+  const storePassword = (e)=>{
+    e.preventDefault();
+    setPassword(e.value);
+  }
+
+  const changeToSignUp = ()=>{
+    navigate('/sign-up')
+  }
+
   return (
     <div>
-        <div>
+        <div className='flex flex-wrap-reverse py-8 justify-around bg-[rgb(37,34,35)] w-[100%]'>
+          <div className='text-white '>
+            <div className='relative py-4'>
+              <p className=' text-white text-[80px] tracking-[-1.984px] font-medium'>LOG IN</p>
+              <p className='text-center pb-10 text-[16px] font-medium absolute bottom-[-24px] left-7'> & Start your Voting journey</p>
+            </div>
+            <label htmlFor='name' className='block pb-2 text-[19px]'>Mobile Number or Email</label>
+            <input id='name' type='text' placeholder='Enter email or mobile number' className='input'/>
+            <label htmlFor='otp' className='block pt-4 pb-2 text-[19px]' >One Time Password(OTP)</label>
+            <input type='text' placeholder='Enter OTP' />
+            <label htmlFor='userId' className='block pt-4 pb-2 text-[19px]'>User ID</label>
+            <input type='text' placeholder='Enter User ID'/>
+            <label htmlFor='password' className='block pt-4 pb-2 text-[19px]'>Password</label>
+            {
+              eyeChecker ? <div className='relative'>
+                <input type="text"  placeholder='Enter password' id='password' onChange={storePassword}/>
+                <AiFillEye className='text-[30px] absolute top-3 cursor-pointer text-black right-3' onClick={changeEye}/>
+              </div> : <div className='relative'>
+                <input type="password" placeholder='Enter password' id='password' onChange={storePassword}/>
+                <AiFillEyeInvisible className='text-[30px] absolute top-3 cursor-pointer text-black right-3' onClick={changeEye} />
+              </div>
+            }
+            <div className='flex justify-center'>
+              <button className='bg-[#6BF2A1] rounded font-normal text-[17px] py-2 mt-5 text-center px-4 text-black'>LOG IN</button>
+            </div>
+            <div className='mt-5 flex items-center gap-3'>
+              <p className='w-[45%] h-[2px] bg-white'></p>
+              <p className='text-[25px]'> OR </p>
+              <p className='w-[45%] h-[2px] bg-white'></p>
+            </div>
+            <div className='flex justify-center'>
+              <button onClick={changeToSignUp} className='bg-[#6BF2A1] rounded font-normal text-[17px] py-2 mt-5 text-center px-4 text-black'>SIGN UP</button>
+            </div>
+          </div>
 
-          
+          <div>
+            <img src={loginIcon} />
+            <img src={loginWoman}  />
+          </div>
         </div>
+        
         <Footer/>
     </div>
   )
--- ui/src/components/Signup.js
+++ ui/src/components/Signup.js
@@ -1,11 +1,65 @@
 import React from 'react';
 import Footer from "./Footer";
-import Navbar from "./Navbar";
+import { useNavigate } from 'react-router-dom';
 
 const Signup = () => {
+  const navigate = useNavigate();
+
+  const changeToLogin = ()=>{
+    navigate('/login');
+  }
+
   return (
     <div>
-        <p>Signup</p>
+        <div className='bg-[rgb(37,34,35)] flex flex-col justify-center pb-28 items-center text-white'>
+          <div className='pb-[50px]'>
+            <h1 className='text-[80px]'>Sign-up</h1>
+            <p className='sign-up h-[6px]'></p>
+          </div>
+          <div>
+            <label htmlFor='name' className='block pb-2 text-[19px]'>Full Name : </label>
+            <input id='name' type='text' placeholder='Enter full name'/>
+            <label htmlFor='mobile' className='block pb-2 pt-4 text-[19px]'>Mobile Number : </label>
+            <input id='mobile' type='text' placeholder='Enter mobile number'/>
+            <label htmlFor='email' className='block pb-2 pt-4 text-[19px]'>Email :</label>
+            <input id='email' type='email' placeholder='Enter email id'/>
+            <label htmlFor='gender' className='block pb-2 pt-4 text-[19px]'>Gender : </label>
+            <select id='gender' className='text-black outline-none bg-white text-[24px] px-[8px] py-[8px] opacity-80 min-w-[300px]'>
+              <option value='Choose your gender'>Choose your gender</option>
+              <option value='Male'>Male</option>
+              <option value='Female'>Female</option>
+              <option value='Do not want to disclose'>Do not want to disclose</option>
+            </select>
+            <label htmlFor='address' className='block pb-2 pt-4 text-[19px]'>Address : </label>
+            <textarea cols='40' rows='5' className='bg-white opacity-80 px-2 py-2 outline-none text-xl text-black' placeholder='Write your address'></textarea>
+          </div>
+          <div className='flex flex-col justify-center items-center'>
+            <label htmlFor='voterId' className='block pb-2 pt-4 text-[30px]'>Voter-ID Card : </label>
+            <input type='file' className='w-[250px]'/>
+            <label htmlFor='voterId' className='block pb-2 pt-4 text-[30px]'>Aadhar Card : </label>
+            <input type='file' className='w-[250px]'/>
+            <label htmlFor='voterId' className='block pb-2 pt-4 text-[30px]'>Pan Card : </label>
+            <input type='file' className='w-[250px]'/>
+          </div>
+
+          <div className='flex mt-3 justify-center'>
+            <button className='bg-[#6BF2A1] rounded font-normal text-[17px] py-2 mt-5 text-center px-4 text-black'>SIGN UP</button>
+          </div>
+          <div className='mt-5 flex items-center gap-3'>
+            <p className='w-[45px] h-[2px] bg-white'></p>
+            <p className='text-[25px]'> OR </p>
+            <p className='w-[45px] h-[2px] bg-white'></p>
+          </div>
+          <div className='flex justify-center'>
+            <button onClick={changeToLogin} className='bg-[#6BF2A1] rounded font-normal text-[17px] py-2 mt-5 text-center px-4 text-black'>LOGIN</button>
+          </div>
+          <div className='w-[240px] py-4 text-[9px] font-normal'>
+            <p>&#42; People who are using our service may need to uploaded the contact information to Election Commission Of India.</p>
+            <p>Learn more</p>
+            <p>&#42; By signing up, you are agree to our Terms, Privacy Policy</p>
+          </div>
+        </div>
+
         <Footer/>
     </div>
   )
